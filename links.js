96eff1de0bbc0565d07eb12306965aaad93acf47
Ravi Kumar8 <164355804+rku8@users.noreply.github.com>
2024-09-21 12:40:53 +0530

    Add files via upload

---
+++ links.js
@@ -0,0 +1,13 @@
+const links = [
+    "https://www.kdnuggets.com/10-github-repositories-to-master-mlops",
+    "https://alumni.iitj.ac.in/profile/3983700",
+    "https://www.freecodecamp.org/news/create-a-wordpress-store-that-sells-real-ai-generated-products/?trk=feed_main-feed-card_feed-article-content",
+    "https://www.jagran.com/web-stories/remember-these-things-of-swami-vivekananda-for-success-69414.html",
+    "https://issuu.com/cmb321/docs/deep_learning_ebook",
+    "https://sebastianraschka.com/Articles/2014_python_lda.html",
+    "https://landing.ai/",
+    "https://coggle.it/diagram/ZO5EUOut86Irr5hc/t/computer-vision-roadmap/760d909a1f28af1782645ff9b5af1dfd4481ce08bf258b4b54f868d7f3a1b8d5",
+    "https://huggingface.co/spaces/ravi46931/textToSQLGenAI",
+    "https://gravatar.com/ravikumar46931",
+    "https://www.timesnownews.com/web-stories/spiritual/inspirational-quotes-by-swami-vivekananda/photostory/110895978.cms"
+  ];
